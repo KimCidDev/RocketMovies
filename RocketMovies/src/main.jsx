3ede5eeccae6ba6300671f73bc2c4870ebbeaa91
Kim <cid.kim@gmail.com>
2023-06-06 17:28:26 -0600

    SignIn Page basic structure

--- RocketMovies/src/main.jsx
+++ RocketMovies/src/main.jsx
@@ -7,13 +7,13 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 
 // While !routes
-import { Profile } from './pages/Profile';
+import { SignIn } from './pages/SignIn';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <Profile />
+    <SignIn />
 
     </ThemeProvider>
   </React.StrictMode>,
