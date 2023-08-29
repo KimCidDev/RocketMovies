dac6ec391c5b5453527ead55a7ab3cb52bd83c76
Kim <cid.kim@gmail.com>
2023-08-29 15:05:06 -0600

    fixing bugs related to node_modules

--- RocketMovies/src/pages/SignUp/index.jsx
+++ RocketMovies/src/pages/SignUp/index.jsx
@@ -5,16 +5,36 @@ import { Container, Background, Form } from './styles';
 import { ButtonText } from '../../components/ButtonText';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
-import { Logo } from '../../components/Logo'
+import { Logo } from '../../components/Logo';
 
+import { api } from '../../services/api';
 
-import { FiMail, FiUserPlus, FiLock } from 'react-icons/fi'
+import { FiMail, FiUserPlus, FiLock } from 'react-icons/fi';
 import { TiArrowLeftThick } from "react-icons/ti";
 
 export function SignUp () {
   const [name, setName] = useState("")
-  const [email, setemail] = useState("")
+  const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+
+  function handleSignUp () {
+    if (!name || !email || !password) {
+      return alert("Não deixe campos vazios, por favor.")
+      
+    }
+
+    api.post('/users', { name, email, password })
+    .then(() => {
+      alert("usuário cadastrado com sucesso")
+    })
+    .catch(error => {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("não foi possível cadastrar");
+      }
+    })
+  }
 
   return (
     <Container>      
@@ -34,14 +54,14 @@ export function SignUp () {
         placeholder="Email"
         type="email"
         icon={FiMail}
-        onChange={(e) => setName(e.target.value)}/>
+        onChange={(e) => setEmail(e.target.value)}/>
       <Input         
         placeholder="Password"
         type="password"
         icon={FiLock}
-        onChange={(e) => setName(e.target.value)}/>
+        onChange={(e) => setPassword(e.target.value)}/>
 
-      <Button title='Entrar'/>
+      <Button title='Cadastrar' onClick={handleSignUp}/>
 
       <ButtonText icon={TiArrowLeftThick}title="Back to Login" />
       </Form>
